e038afe5a0a951b1be5f16887f2ad1831e8f5f43
Taiwo Olawuwo <42567505+olawuwo@users.noreply.github.com>
2024-03-05 16:13:52 +0000

    Exercise 9

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,51 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+
+-- checking for the customer if they exist
+SELECT 
+  *
+FROM 
+  Customers
+WHERE 
+  Email = 'smac@kinetecoinc.com';
+
+-- Unfortunately they do not exist at the time of 
+-- this exercise
+-- A quick check at the columns in the reservation table
+SELECT 
+  *
+FROM 
+  Reservations;
+
+--Inserting the new customer record
+INSERT 
+  INTO Customers (
+  FirstName,
+  LastName,
+  Email) 
+VALUES (
+  'Sam',
+  'McAdams',
+  'smac@kinetecoinc.com'
+  );
+
+-- Making a reservation for the customer (ID=102)
+INSERT 
+INTO Reservations (
+  CustomerID,
+  Date,
+  PartySize) 
+VALUES (
+  '102',
+  '2024-05-03',
+  5
+  );
+
+-- Quick check to confirm the reservation was made
+SELECT 
+  *
+FROM 
+  Reservations
+WHERE
+  CustomerID = 102
--- 01_restaurant/challenges/01_10_takeorder_ch.sql
+++ 01_restaurant/challenges/01_10_takeorder_ch.sql
@@ -7,3 +7,25 @@
 -- 1 Tropical Blue Smoothie
 -- Delivery date and time: September 20, 2022 @ 2PM (14:00)
 -- There are no taxes or other fees.
+
+-- Using the customer address to find the customer, CustomerID=70
+SELECT 
+  *
+FROM 
+  Customers
+WHERE
+  Address = '6939 Elka Place';
+
+SELECT 
+  Customers.FirstName,
+  Customers.LastName,
+  Customers.Address,
+  Dishes.Name,
+  Dishes.Price
+FROM 
+  Orders 
+JOIN OrdersDishes 
+ON Orders.OrderID = OrdersDishes.OrderID
+JOIN Dishes 
+ON Dishes.DishID = OrdersDishes.DishID
+WHERE OrderID = 1 AND Orders.CustomerID = 77;
